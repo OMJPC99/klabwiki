7fb4193f7e1256863f63fff03cf7f1a16b85796a
OMJPC99 <theodoreando99@gmail.com>
2015-07-09 10:13:33 -0500

    Adding paralleized cleaning script

---
+++ swift_clean.swift
@@ -0,0 +1,30 @@
+type file;
+
+# interval_size is the number of files per subset to be passed to workers
+# file_list is the list of split files in the bucket
+# file_interval_count is the number of distinct intervals fit into the file_list
+app (file file_list) get_files (file lister) {
+    get_files @lister @file_list;
+}
+
+# scraper - clean_and_ingest.py
+# 	cleaner - grep_cleaner.py
+#	ingester - UniversalIngest.py
+#	filterer - filter_database.py
+app (file ip_list, file out, file err) final_data_scrape (file scraper, file_cleaner, file filterer) {
+    final_data_scrape @scraper @ip_list stdout=filename(out) stderr=filename(err);
+}
+
+file lister <single_file_mapper; file="get_split_list.py">;
+file file_list <single_file_mapper; file="split_file_list.txt">;
+
+file_list = get_files(lister);
+string split_files[] = readData(file_list);
+
+file scraper <single_file_mapper; file="clean_and_ingest.py">;
+file cleaner <single_file_mapper; file="grep_cleaner.py">;
+file ingester <single_file_mapper; file="UniversalIngest.py">;
+
+foreach split_file,i in split_files {
+    final_data_scrape();
+}
